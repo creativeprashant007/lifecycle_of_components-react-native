ab385631644262c63a34b0394794f143e3129a0b
creativeprashant007 <prashanttimalsina18@gmail.com>
2023-04-10 16:34:03 +0545

    feat:Lifecycle of components

--- App.tsx
+++ App.tsx
@@ -1,118 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { Component, ReactElement } from 'react';
+import { Text, View } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+interface Props {
+  name: string;
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+interface State {}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+class CustomComponent extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    console.log('Constructor Called.');
+  }
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  componentDidMount() {
+    console.log('componentDidMount called.');
+  }
+
+  shouldComponentUpdate(nextProps: Props, nextState: State): boolean {
+    console.log('shouldComponentUpdate called.');
+    return true;
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    console.log('componentDidUpdate called.');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount called.');
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.log('componentDidCatch called.');
+  }
+
+  render(): ReactElement {
+    return (
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Language is: {this.props.name}</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default class App extends Component {
+  render(): ReactElement {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <CustomComponent name="Hello I am prashant timalsina" />
+      </View>
+    );
+  }
+}
